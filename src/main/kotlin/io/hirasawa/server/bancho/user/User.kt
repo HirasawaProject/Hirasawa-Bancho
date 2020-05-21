6e01085ee862aa0e4cfa5470961044bc09bf8b0c
Connor Graham <thecg1997@gmail.com>
2020-05-21 21:17:38 +0100

    If user is banned, set default cancel reason to banned

--- src/main/kotlin/io/hirasawa/server/bancho/user/User.kt
+++ src/main/kotlin/io/hirasawa/server/bancho/user/User.kt
@@ -7,6 +7,6 @@ import io.hirasawa.server.bancho.enums.GameMode
  * A base user, this is used to extend into classes with more features
  */
 abstract class User(val id: Int, val username: String, val timezone: Byte, val countryCode: Byte, val permissions: Byte,
-                    val mode: GameMode, val longitude: Float, val latitude: Float, val banned: Boolean): CommandSender {
+                    val mode: GameMode, val longitude: Float, val latitude: Float, val isBanned: Boolean): CommandSender {
     abstract fun sendPrivateMessage(from: User, message: String)
 }
--- src/main/kotlin/io/hirasawa/server/plugin/event/bancho/BanchoUserLoginEvent.kt
+++ src/main/kotlin/io/hirasawa/server/plugin/event/bancho/BanchoUserLoginEvent.kt
@@ -9,8 +9,7 @@ import io.hirasawa.server.plugin.event.bancho.enums.BanchoLoginCancelReason
  * This occurs after authentication and before the client is sent the response
  *
  */
-data class BanchoUserLoginEvent(val user: User): HirasawaEvent {
-    var cancelReason = BanchoLoginCancelReason.NOT_CANCELLED
+data class BanchoUserLoginEvent(val user: User, var cancelReason: BanchoLoginCancelReason): HirasawaEvent {
     fun cancelLogin(reason: BanchoLoginCancelReason) {
         cancelReason = reason
     }
