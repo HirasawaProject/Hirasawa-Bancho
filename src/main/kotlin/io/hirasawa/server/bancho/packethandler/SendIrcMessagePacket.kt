cf11da6da4144836a0414aa78f5413f6ff6edda2
Connor Graham <thecg1997@gmail.com>
2020-05-23 23:13:44 +0100

    Don't store unused variable

--- src/main/kotlin/io/hirasawa/server/bancho/packethandler/SendIrcMessagePacket.kt
+++ src/main/kotlin/io/hirasawa/server/bancho/packethandler/SendIrcMessagePacket.kt
@@ -8,7 +8,7 @@ import io.hirasawa.server.bancho.user.BanchoUser
 
 class SendIrcMessagePacket: PacketHandler(BanchoPacketType.OSU_SEND_IRC_MESSAGE) {
     override fun handle(reader: OsuReader, writer: OsuWriter, user: BanchoUser) {
-        val messageFrom = reader.readString() // We're not gonna use this since we have the user anyway
+        reader.readString() // Message from, we're not gonna use this since we have the user anyway
         val message = reader.readString()
         val channel = reader.readString()
 
