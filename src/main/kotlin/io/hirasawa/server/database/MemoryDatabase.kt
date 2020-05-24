fa57c49566b62e38f5b12fd9aaadad231f7a3037
Connor Graham <thecg1997@gmail.com>
2020-05-24 15:27:06 +0100

    Add new functions to memory database

--- src/main/kotlin/io/hirasawa/server/database/MemoryDatabase.kt
+++ src/main/kotlin/io/hirasawa/server/database/MemoryDatabase.kt
@@ -1,9 +1,12 @@
 package io.hirasawa.server.database
 
+import io.hirasawa.server.bancho.enums.GameMode
 import io.hirasawa.server.bancho.user.User
+import io.hirasawa.server.objects.Beatmap
+import io.hirasawa.server.objects.BeatmapSet
+import io.hirasawa.server.objects.Score
 import io.hirasawa.server.permissions.PermissionGroup
 import java.lang.Exception
-import java.util.ArrayList
 import java.util.HashMap
 
 /**
@@ -14,6 +17,10 @@ class MemoryDatabase(): Database(DatabaseCredentials()) {
     val users = ArrayList<User>()
     val friends = HashMap<Int, ArrayList<User>>()
     val groups = HashMap<String, PermissionGroup>()
+    val scores = ArrayList<Score>()
+    val beatmaps = ArrayList<Beatmap>()
+    val beatmapSets = ArrayList<BeatmapSet>()
+
 
 
     override fun authenticate(username: String, password: String): Boolean {
@@ -54,5 +61,68 @@ class MemoryDatabase(): Database(DatabaseCredentials()) {
 
     override fun getPermissionGroups(): HashMap<String, PermissionGroup> {
         return groups
+    }
+
+    override fun getScore(id: Int): Score? {
+        for (score in scores) {
+            if (score.id == id) {
+                return score
+            }
+        }
+
+        return null
+    }
+
+    override fun getBeatmap(id: Int): Beatmap? {
+        for (beatmap in beatmaps) {
+            if (beatmap.id == id) {
+                return beatmap
+            }
+        }
+
+        return null
+    }
+
+    override fun getBeatmap(hash: String): Beatmap? {
+        for (beatmap in beatmaps) {
+            if (beatmap.hash == hash) {
+                return beatmap
+            }
+        }
+
+        return null
+    }
+
+    override fun getBeatmapSet(id: Int): BeatmapSet? {
+        for (beatmapSet in beatmapSets) {
+            if (beatmapSet.id == id) {
+                return beatmapSet
+            }
+        }
+
+        return null
+    }
+
+    override fun getBeatmapScores(beatmap: Beatmap, mode: GameMode, limit: Int): ArrayList<Score> {
+        val listedScores = ArrayList<Score>()
+        for (score in scores) {
+            if (score.beatmapId == beatmap.id) {
+                listedScores.add(score)
+            }
+        }
+
+        scores.sortBy { it.score }
+
+        return listedScores
+    }
+
+    override fun getUserScore(beatmap: Beatmap, mode: GameMode, user: User): Score? {
+        for (score in scores) {
+            if (score.user == user && score.beatmapId == beatmap.id) {
+                return score
+            }
+        }
+
+        return null
     }
 }
