9d16ca5ab4d4a0192d90bebac2c9ac4b4e0fd19d
Connor Graham <thecg1997@gmail.com>
2020-06-04 17:15:53 +0100

    Create folder if doesn't exist

--- src/main/kotlin/io/hirasawa/server/logger/FileLogger.kt
+++ src/main/kotlin/io/hirasawa/server/logger/FileLogger.kt
@@ -4,7 +4,13 @@ import java.io.File
 import java.io.FileWriter
 
 open class FileLogger(file: File): Logger() {
+    init {
+        if (!file.parentFile.exists()) {
+            file.parentFile.mkdirs()
+        }
+    }
     private val writer = FileWriter(file, true)
+
     override fun log(message: Any) {
         writer.write("$timestamp $message\n")
         writer.flush()
