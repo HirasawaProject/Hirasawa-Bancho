edb6a32e6cda3133ea1bcd3e06f1088380a13de9
Connor Graham <thecg1997@gmail.com>
2020-06-21 00:13:37 +0100

    Add description and permission data to ChatCommand

--- src/main/kotlin/io/hirasawa/server/bancho/chat/command/ChatCommand.kt
+++ src/main/kotlin/io/hirasawa/server/bancho/chat/command/ChatCommand.kt
@@ -1,5 +1,5 @@
 package io.hirasawa.server.bancho.chat.command
 
-abstract class ChatCommand(val name: String) {
+abstract class ChatCommand(val name: String, val description: String, val permission: String = "") {
     abstract fun onCommand(context: CommandContext, command: String, args: List<String>): Boolean
 }
--- src/main/kotlin/io/hirasawa/server/commands/TestCommand.kt
+++ src/main/kotlin/io/hirasawa/server/commands/TestCommand.kt
@@ -7,7 +7,7 @@ import io.hirasawa.server.bancho.chat.message.PrivateChatMessage
 import io.hirasawa.server.bancho.packets.SendMessagePacket
 import io.hirasawa.server.bancho.user.BanchoUser
 
-class TestCommand: ChatCommand("test") {
+class TestCommand: ChatCommand("test", "Test command", "hirasawa.command.test") {
     override fun onCommand(context: CommandContext, command: String, args: List<String>): Boolean {
         if (context.sender is BanchoUser) {
             context.respond("This is a test")
