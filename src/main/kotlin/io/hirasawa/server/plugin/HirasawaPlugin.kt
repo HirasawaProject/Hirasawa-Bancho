ac7f6e009f1f11313deb246a30c8c584c48a4b09
Connor Graham <thecg1997@gmail.com>
2020-06-22 17:21:55 +0100

    Add KDoc

--- src/main/kotlin/io/hirasawa/server/plugin/HirasawaPlugin.kt
+++ src/main/kotlin/io/hirasawa/server/plugin/HirasawaPlugin.kt
@@ -9,10 +9,20 @@ abstract class HirasawaPlugin: IHirasawaPlugin {
     lateinit var pluginDescriptor: PluginDescriptor
     lateinit var logger: PluginLogger
 
+    /**
+     * Register an event class to the event handler
+     *
+     * @param eventListener The event listener instance to be registered
+     */
     fun registerEvent(eventListener: EventListener) {
         Hirasawa.eventHandler.registerEvent(eventListener)
     }
 
+    /**
+     * Register a command to the chat engine
+     *
+     * @param chatCommand The command instance to be registered
+     */
     fun registerCommand(chatCommand: ChatCommand) {
         Hirasawa.chatEngine.chatCommands[chatCommand.name] = chatCommand
     }
