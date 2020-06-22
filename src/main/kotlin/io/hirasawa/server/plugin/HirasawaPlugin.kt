0ccc6776372a6275ec16a6d4d082a56bc76548f5
Connor Graham <thecg1997@gmail.com>
2020-06-22 17:15:12 +0100

    Add helper plugin functions

--- src/main/kotlin/io/hirasawa/server/plugin/HirasawaPlugin.kt
+++ src/main/kotlin/io/hirasawa/server/plugin/HirasawaPlugin.kt
@@ -1,8 +1,19 @@
 package io.hirasawa.server.plugin
 
+import io.hirasawa.server.Hirasawa
+import io.hirasawa.server.bancho.chat.command.ChatCommand
 import io.hirasawa.server.logger.PluginLogger
+import io.hirasawa.server.plugin.event.EventListener
 
 abstract class HirasawaPlugin: IHirasawaPlugin {
     lateinit var pluginDescriptor: PluginDescriptor
     lateinit var logger: PluginLogger
+
+    fun registerEvent(eventListener: EventListener) {
+        Hirasawa.eventHandler.registerEvent(eventListener)
+    }
+
+    fun registerCommand(chatCommand: ChatCommand) {
+        Hirasawa.chatEngine.chatCommands[chatCommand.name] = chatCommand
+    }
 }
