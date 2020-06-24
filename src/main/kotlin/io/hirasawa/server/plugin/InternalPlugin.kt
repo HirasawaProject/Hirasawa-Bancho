b0a28cd78678dcf6ee62d76818565d90d42e10c9
Connor Graham <thecg1997@gmail.com>
2020-06-24 02:19:40 +0100

    Add reload command

--- src/main/kotlin/io/hirasawa/server/plugin/InternalPlugin.kt
+++ src/main/kotlin/io/hirasawa/server/plugin/InternalPlugin.kt
@@ -1,12 +1,14 @@
 package io.hirasawa.server.plugin
 
 import io.hirasawa.server.commands.HelpCommand
+import io.hirasawa.server.commands.ReloadCommand
 import io.hirasawa.server.commands.TestCommand
 
 class InternalPlugin: HirasawaPlugin() {
     override fun onEnable() {
         registerCommand(TestCommand())
         registerCommand(HelpCommand())
+        registerCommand(ReloadCommand())
     }
 
     override fun onDisable() {
