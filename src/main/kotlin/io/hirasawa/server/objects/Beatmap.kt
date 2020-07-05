6a72e4d493888ccdb6860fa90fb2b3c606b96d57
Connor Graham <thecg1997@gmail.com>
2020-07-05 16:18:00 +0100

    Change variables from osu! API names to be more readable

--- src/main/kotlin/io/hirasawa/server/objects/Beatmap.kt
+++ src/main/kotlin/io/hirasawa/server/objects/Beatmap.kt
@@ -9,19 +9,20 @@ import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 data class Beatmap(var id: Int, var mapsetId: Int, var difficulty: String, var hash: String, var ranks: Int,
-                   var offset: Float, val osuId: Int, val totalLength: Int, val hitLength: Int, val diffSize: Float,
-                   val diffOverall: Float, val diffApproach: Float, val diffDrain: Float, val mode: GameMode,
-                   val countNormal: Int, val countSlider: Int, val countSpinner: Int, val bpm: Float,
-                   val hasStoryboard: Boolean, val maxCombo: Int, val playCount: Int, val passCount: Int) {
+                   var offset: Float, val osuId: Int, val totalLength: Int, val hitLength: Int, val circleSize: Float,
+                   val overallDifficulty: Float, val approachRate: Float, val healthDrain: Float,
+                   val gamemode: GameMode, val countNormal: Int, val countSlider: Int, val countSpinner: Int,
+                   val bpm: Float, val hasStoryboard: Boolean, val maxCombo: Int, val playCount: Int,
+                   val passCount: Int) {
 
     constructor(result: ResultRow): this(result[BeatmapsTable.id].value, result[BeatmapsTable.mapsetId],
         result[BeatmapsTable.difficulty], result[BeatmapsTable.hash], result[BeatmapsTable.ranks],
         result[BeatmapsTable.offset], result[BeatmapsTable.osuId], result[BeatmapsTable.totalLength],
-        result[BeatmapsTable.hitLength], result[BeatmapsTable.diffSize], result[BeatmapsTable.diffOverall],
-        result[BeatmapsTable.diffApproach], result[BeatmapsTable.diffDrain],
-        GameMode.values()[result[BeatmapsTable.mode]], result[BeatmapsTable.countNormal],
+        result[BeatmapsTable.hitLength], result[BeatmapsTable.circleSize], result[BeatmapsTable.overallDifficulty],
+        result[BeatmapsTable.approachRate], result[BeatmapsTable.healthDrain],
+        GameMode.values()[result[BeatmapsTable.gamemode]], result[BeatmapsTable.countNormal],
         result[BeatmapsTable.countSlider], result[BeatmapsTable.countSpinner], result[BeatmapsTable.bpm],
-        result[BeatmapsTable.storyboard], result[BeatmapsTable.maxCombo], result[BeatmapsTable.playCount],
+        result[BeatmapsTable.hasStoryboard], result[BeatmapsTable.maxCombo], result[BeatmapsTable.playCount],
         result[BeatmapsTable.passCount])
 
     val beatmapSet by lazy {
