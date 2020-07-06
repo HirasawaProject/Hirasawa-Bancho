40ee7ced5737ea453f508042e385e386d3d5719d
Connor Graham <thecg1997@gmail.com>
2020-07-06 01:31:26 +0100

    Make automated way to rank beatmaps

--- src/main/kotlin/io/hirasawa/server/database/tables/BeatmapsetsTable.kt
+++ src/main/kotlin/io/hirasawa/server/database/tables/BeatmapsetsTable.kt
@@ -3,7 +3,7 @@ package io.hirasawa.server.database.tables
 import org.jetbrains.exposed.dao.IntIdTable
 
 object BeatmapsetsTable: IntIdTable("beatmapsets") {
-    val osuId = integer("osu_id").uniqueIndex()
+    val osuId = integer("osu_id")
     val artist = varchar("artist", 255)
     val title = varchar("title", 255)
     val status = integer("status")
