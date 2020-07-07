93f6148433dfb6ffe9e6fd5fea14eba331ec52c1
Connor Graham <thecg1997@gmail.com>
2020-07-07 18:44:31 +0100

    Add pagination to help command #156

--- src/main/kotlin/io/hirasawa/server/commands/HelpCommand.kt
+++ src/main/kotlin/io/hirasawa/server/commands/HelpCommand.kt
@@ -4,23 +4,34 @@ import io.hirasawa.server.Hirasawa
 import io.hirasawa.server.bancho.chat.command.ChatCommand
 import io.hirasawa.server.bancho.chat.command.CommandContext
 import io.hirasawa.server.bancho.user.User
+import io.hirasawa.server.helpers.paginate
 
 class HelpCommand: ChatCommand("help", "Lists the commands you are able to do") {
     override fun onCommand(context: CommandContext, command: String, args: List<String>): Boolean {
-        val commands = Hirasawa.chatEngine.chatCommands
+        val commands = Hirasawa.chatEngine.chatCommands.values.toList().paginate(10)
 
-        if (context.sender is User) {
-            for (chatCommand in commands.values) {
+        var page = 1
+
+        if (args.isNotEmpty()) {
+            page = args[0].toInt()
+        }
+
+        context.respond("Page $page of ${commands.size}")
+
+        if (page > commands.size || page < 1) {
+            context.respond("Page doesn't exist!")
+            return false
+        }
+
+        for (chatCommand in commands[page - 1]) {
+            if (context.sender is User) {
                 if (context.sender.hasPermission(chatCommand.first.permission)) {
                     context.respond(getCommandString(chatCommand.first))
                 }
-            }
-        } else {
-            for (chatCommand in commands.values) {
+            } else {
                 context.respond(getCommandString(chatCommand.first))
             }
         }
-
 
         return false
     }
