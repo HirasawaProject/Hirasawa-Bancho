c59ac9238bc55147a82014810fb4a0d823fe04cb
Connor Graham <thecg1997@gmail.com>
2020-07-07 18:30:15 +0100

    Remove debug #143

--- src/test/kotlin/io/hirasawa/server/helpers/HelperTests.kt
+++ src/test/kotlin/io/hirasawa/server/helpers/HelperTests.kt
@@ -8,8 +8,6 @@ class HelperTests {
     fun testListPaginate() {
         val list = listOf("This", "is", "a", "test", "for", "the", "pagination", "helper").paginate(3)
 
-        println(list)
-
         assertEquals(3, list.size)
         assertEquals(3, list.first().size)
         assertEquals(2, list.last().size)
