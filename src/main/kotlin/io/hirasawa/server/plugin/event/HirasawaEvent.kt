88db49fc919ae99f7d4fa5af26dbb0af87b1b001
Connor Graham <thecg1997@gmail.com>
2022-02-27 19:15:20 +0000

    Create seperate event call class to handle the then/cancelled functions

--- src/main/kotlin/io/hirasawa/server/plugin/event/HirasawaEvent.kt
+++ src/main/kotlin/io/hirasawa/server/plugin/event/HirasawaEvent.kt
@@ -3,28 +3,8 @@ package io.hirasawa.server.plugin.event
 import io.hirasawa.server.Hirasawa
 
 interface HirasawaEvent <out T: HirasawaEvent<T>> {
-    fun call(): T {
+    fun call(): HirasawaEventCall<T> {
         Hirasawa.eventHandler.callEvent(this)
-        return this as T
-    }
-
-    fun then(body: (event: T) -> Unit): T {
-        if (this is Cancelable) {
-            if (!isCancelled) {
-                body.invoke(this as T)
-            }
-        } else {
-            body.invoke(this as T)
-        }
-        return this as T
-    }
-
-    fun cancelled(body: (event: T) -> Unit): T {
-        if (this is Cancelable) {
-            if (isCancelled) {
-                body.invoke(this as T)
-            }
-        }
-        return this as T
+        return HirasawaEventCall(this as T)
     }
 }
