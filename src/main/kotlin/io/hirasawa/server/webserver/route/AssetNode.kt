ad0b0e5ab5ca5f853ee2e9b707d39f4838dfea24
Connor Graham <thecg1997@gmail.com>
2022-03-01 18:14:07 +0000

    Allow asset node to flush automatically every 1000 bytes

--- src/main/kotlin/io/hirasawa/server/webserver/route/AssetNode.kt
+++ src/main/kotlin/io/hirasawa/server/webserver/route/AssetNode.kt
@@ -8,9 +8,6 @@ import io.hirasawa.server.webserver.objects.Response
 import java.io.File
 import java.nio.file.Files
 
-
-
-
 class AssetNode(val assetPath: String): RouteNode {
     override fun handle(method: HttpMethod, path: List<String>, request: Request, response: Response) {
         if (File(assetPath).exists()) {
@@ -18,7 +15,17 @@ class AssetNode(val assetPath: String): RouteNode {
             val mimeType = Files.probeContentType(filePath)
 
             response.headers[HttpHeader.CONTENT_TYPE] = mimeType
-            response.outputStream.write(Files.readAllBytes(filePath))
+
+            val bytes = Files.readAllBytes(filePath)
+
+            response.headers[HttpHeader.CONTENT_LENGTH] = bytes.size
+
+            for (i in 0..bytes.size) {
+                if (i % 1000 == 0) {
+                    response.flush()
+                }
+                response.outputStream.writeByte(bytes[i].toInt())
+            }
             response.close()
         } else {
             RouteNotFoundRoute().handle(request, response)
