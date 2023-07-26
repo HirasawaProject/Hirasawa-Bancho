dfea70652533d013b0b8664d9468a1a2d44b99ac
Connor Graham <thecg1997@gmail.com>
2023-07-26 05:57:01 +0100

    Add free mods to config

--- src/main/kotlin/io/hirasawa/server/config/HirasawaConfig.kt
+++ src/main/kotlin/io/hirasawa/server/config/HirasawaConfig.kt
@@ -3,11 +3,12 @@ package io.hirasawa.server.config
 import io.hirasawa.server.chat.ChatChannel
 import io.hirasawa.server.database.DatabaseCredentials
 import io.hirasawa.server.enums.Mod
+import io.hirasawa.server.objects.Mods
 
 data class HirasawaConfig (val httpPort: Int, val httpsPort: Int, val ircPort: Int, val database: DatabaseCredentials,
                            val channels: List<ChatChannel>, val banchoUserTimeout: Int, val ircUserTimeout: Int,
                            val banchoBotId: Int, val domain: String, val blockedMods: List<Mod>, val osuApiKey: String,
-                           val ircWelcomeMessage: String, val ircMotd: List<String>) {
+                           val ircWelcomeMessage: String, val ircMotd: List<String>, val multiplayerFreeMods: Mods) {
     constructor(): this(
         8080,
         4430,
@@ -27,6 +28,10 @@ data class HirasawaConfig (val httpPort: Int, val httpsPort: Int, val ircPort: I
         listOf(
             "This is an example of a MOTD for the IRC server",
             "set this to whatever you want"
+        ),
+        Mods(
+            arrayListOf(Mod.NO_FAIL, Mod.EASY, Mod.HIDDEN, Mod.HARD_ROCK, Mod.SUDDEN_DEATH, Mod.FLASHLIGHT, Mod.RELAX,
+            Mod.AUTOPILOT, Mod.SPUN_OUT, Mod.KEY_MODS)
         )
     )
 }
