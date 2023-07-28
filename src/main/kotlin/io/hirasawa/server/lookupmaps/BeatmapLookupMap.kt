877e9accef19431d6cc8bc856fe274a6a646f62d
Connor Graham <thecg1997@gmail.com>
2023-07-28 04:26:15 +0100

    Change lookup maps to use centralised rankBeatmapSet function

--- src/main/kotlin/io/hirasawa/server/lookupmaps/BeatmapLookupMap.kt
+++ src/main/kotlin/io/hirasawa/server/lookupmaps/BeatmapLookupMap.kt
@@ -24,45 +24,8 @@ class BeatmapLookupMap: LookupMap<Beatmap>(Beatmap::class, BeatmapsTable.hash, B
         if (queriedBeatmap.isEmpty()) return null
         val beatmaps = Hirasawa.osuApi.getBeatmaps(mapsetId = queriedBeatmap.first().beatmapsetId)
         if (beatmaps.isNotEmpty()) {
-            transaction {
-                val setId = BeatmapSetsTable.insertAndGetId {
-                    it[artist] = beatmaps.first().artist
-                    it[title] = beatmaps.first().title
-                    it[status] = BeatmapStatus.RANKED.ordinal
-                    it[osuId] = beatmaps.first().beatmapsetId
-                    it[mapperName] = beatmaps.first().creator
-                    it[genreId] = beatmaps.first().genreId
-                    it[languageId] = beatmaps.first().languageId
-                    it[rating] = beatmaps.first().rating
-                }.value
-
-                commit()
-
-                for (beatmap in beatmaps) {
-                    BeatmapsTable.insert {
-                        it[mapsetId] = setId
-                        it[difficulty] = beatmap.version
-                        it[hash] = beatmap.fileMd5
-                        it[ranks] = 0
-                        it[offset] = 0F
-                        it[osuId] = beatmap.beatmapId
-                        it[totalLength] = beatmap.totalLength
-                        it[hitLength] = beatmap.hitLength
-                        it[circleSize] = beatmap.diffSize
-                        it[overallDifficulty] = beatmap.diffOverall
-                        it[approachRate] = beatmap.diffApproach
-                        it[healthDrain] = beatmap.diffDrain
-                        it[gamemode] = beatmap.mode
-                        it[countNormal] = beatmap.countNormal
-                        it[countSlider] = beatmap.countSlider
-                        it[countSpinner] = beatmap.countSpinner
-                        it[bpm] = beatmap.bpm
-                        it[hasStoryboard] = beatmap.storyboard
-                        it[maxCombo] = beatmap.maxCombo
-                        it[playCount] = 0
-                        it[passCount] = 0
-                    }
-                }
+            if (!Hirasawa.rankBeatmapSet(beatmaps.first().beatmapsetId)) {
+                return null
             }
 
             return if (key != null) {
--- src/main/kotlin/io/hirasawa/server/lookupmaps/BeatmapSetLookupMap.kt
+++ src/main/kotlin/io/hirasawa/server/lookupmaps/BeatmapSetLookupMap.kt
@@ -2,12 +2,7 @@ package io.hirasawa.server.lookupmaps
 
 import io.hirasawa.server.Hirasawa
 import io.hirasawa.server.database.tables.BeatmapSetsTable
-import io.hirasawa.server.database.tables.BeatmapsTable
-import io.hirasawa.server.enums.BeatmapStatus
 import io.hirasawa.server.objects.BeatmapSet
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.insertAndGetId
-import org.jetbrains.exposed.sql.transactions.transaction
 
 class BeatmapSetLookupMap: LookupMap<BeatmapSet>(BeatmapSet::class, BeatmapSetsTable.title, BeatmapSetsTable.osuId) {
     override fun getKey(obj: BeatmapSet): String {
@@ -21,51 +16,15 @@ class BeatmapSetLookupMap: LookupMap<BeatmapSet>(BeatmapSet::class, BeatmapSetsT
     override fun lookupExternalObject(key: String?, id: Int?): BeatmapSet? {
         val beatmaps = Hirasawa.osuApi.getBeatmaps(mapsetId = id)
         if (beatmaps.isNotEmpty()) {
-            var setId = 0
-            transaction {
-                setId = BeatmapSetsTable.insertAndGetId {
-                    it[artist] = beatmaps.first().artist
-                    it[title] = beatmaps.first().title
-                    it[status] = BeatmapStatus.RANKED.ordinal
-                    it[osuId] = beatmaps.first().beatmapsetId
-                    it[mapperName] = beatmaps.first().creator
-                    it[genreId] = beatmaps.first().genreId
-                    it[languageId] = beatmaps.first().languageId
-                    it[rating] = beatmaps.first().rating
-                }.value
+            Hirasawa.rankBeatmapSet(beatmaps.first().beatmapsetId)
 
-                commit()
-
-                for (beatmap in beatmaps) {
-                    BeatmapsTable.insert {
-                        it[mapsetId] = setId
-                        it[difficulty] = beatmap.version
-                        it[hash] = beatmap.fileMd5
-                        it[ranks] = 0
-                        it[offset] = 0F
-                        it[osuId] = beatmap.beatmapId
-                        it[totalLength] = beatmap.totalLength
-                        it[hitLength] = beatmap.hitLength
-                        it[circleSize] = beatmap.diffSize
-                        it[overallDifficulty] = beatmap.diffOverall
-                        it[approachRate] = beatmap.diffApproach
-                        it[healthDrain] = beatmap.diffDrain
-                        it[gamemode] = beatmap.mode
-                        it[countNormal] = beatmap.countNormal
-                        it[countSlider] = beatmap.countSlider
-                        it[countSpinner] = beatmap.countSpinner
-                        it[bpm] = beatmap.bpm
-                        it[hasStoryboard] = beatmap.storyboard
-                        it[maxCombo] = beatmap.maxCombo
-                        it[playCount] = 0
-                        it[passCount] = 0
-                    }
-                }
+            return if (key != null) {
+                this[key]
+            } else if (id != null) {
+                this[id]
+            } else {
+                null
             }
-
-            return BeatmapSet(setId, beatmaps.first().artist, beatmaps.first().title, BeatmapStatus.RANKED,
-                    beatmaps.first().beatmapsetId, beatmaps.first().creator, beatmaps.first().genreId,
-                    beatmaps.first().languageId, beatmaps.first().rating)
         }
         return null
     }
