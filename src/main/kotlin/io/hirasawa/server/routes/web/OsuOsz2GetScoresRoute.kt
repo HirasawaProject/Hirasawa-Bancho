08dc75c1ef057920b0735c0074387e25cc98c428
Connor Graham <thecg1997@gmail.com>
2023-07-28 04:23:23 +0100

    Change score lookup to use new beatmap cachemap

--- src/main/kotlin/io/hirasawa/server/routes/web/OsuOsz2GetScoresRoute.kt
+++ src/main/kotlin/io/hirasawa/server/routes/web/OsuOsz2GetScoresRoute.kt
@@ -3,14 +3,12 @@ package io.hirasawa.server.routes.web
 import io.hirasawa.server.Hirasawa
 import io.hirasawa.server.bancho.enums.GameMode
 import io.hirasawa.server.bancho.user.BanchoUser
-import io.hirasawa.server.database.tables.BeatmapsTable
 import io.hirasawa.server.database.tables.ScoresTable
 import io.hirasawa.server.database.tables.UsersTable
 import io.hirasawa.server.enums.BeatmapStatus
 import io.hirasawa.server.handlers.GetScoresErrorHeaderHandler
 import io.hirasawa.server.handlers.GetScoresHeaderHandler
 import io.hirasawa.server.handlers.ScoreInfoHandler
-import io.hirasawa.server.objects.Beatmap
 import io.hirasawa.server.objects.Score
 import io.hirasawa.server.plugin.event.score.ClientLeaderboardFailEvent
 import io.hirasawa.server.plugin.event.score.ClientLeaderboardLoadEvent
@@ -49,9 +47,7 @@ class OsuOsz2GetScoresRoute: Route {
 
             ClientLeaderboardPreloadEvent(user, beatmapHash, gamemode).call()
 
-            val beatmap = Hirasawa.databaseToObject<Beatmap>(Beatmap::class, transaction {
-                BeatmapsTable.select { BeatmapsTable.hash eq beatmapHash }.firstOrNull()
-            })
+            val beatmap = Hirasawa.beatmaps[beatmapHash]
             val beatmapSet = beatmap?.beatmapSet
 
             if (beatmap == null || beatmapSet == null) {
